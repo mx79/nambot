6ed8fccf0ddf2fc07c988fe4f337f199ac568330
Max Lesage <lesage.max@sfr.fr>
2022-09-21 11:41:47 +0200

    Delete script directory

--- script/test.py
+++
@@ -1,8 +0,0 @@
-from googletrans import Translator
-
-translator = Translator()
-
-print(translator.translate("I'm doing some tests on this app", dest="fr"))
-
-print(translator.detect("I'm doing some tests on this app"))
-
