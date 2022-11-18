e24874d1c56352cd7a6bf60046eb314dfb5a1024
mx79 <lesage.max@sfr.fr>
2022-11-18 13:43:49 +0100

    update

--- static/js/chatbot.js
+++ static/js/chatbot.js
@@ -1,61 +1,28 @@
-import Recorder from "recorderjs";
-require('dotenv').config();
-
 // TTS integration
 let vocal = new SpeechSynthesisUtterance()
 vocal.lang = "fr";
 
 // STT Integration: audio stream
-let recording;
-let gumStream;
-let audioInput;
-let rec;
-const AudioContext = window.AudioContext || window.webkitAudioContext;
-let audioContext = new AudioContext;
-
-/**
- * Call the whisper STT api with an audio input
- * @returns {Promise<any>}
- */
-async function queryWhisperStt(filename) {
-    const fs = require('fs')
-    const data = fs.readFileSync(filename);
-    const response = await fetch(
-        "https://api-inference.huggingface.co/models/openai/whisper-tiny",
-        {
-            headers: {Authorization: "Bearer " + process.env.STT_API_KEY},
-            method: "POST",
-            body: data,
-        }
-    );
-    return await response.json();
-}
+const SpeechRecognition = window.webkitSpeechRecognition
+let recognition = new SpeechRecognition()
+recognition.continuous = false;
+recognition.lang = 'fr';
 
 /**
  * Gets the local audio stream of the current caller
  * If user accept the audio streaming, return true, else false
- * @returns {void}
+ * @returns {boolean}
  */
 function getLocalStream() {
     navigator.mediaDevices.getUserMedia({video: false, audio: true}).then((stream) => {
         window.localStream = stream;
         window.localAudio.srcObject = stream;
         window.localAudio.autoplay = true;
-        // assign to gumStream for later use
-        gumStream = stream;
-        // use the stream
-        audioInput = audioContext.createMediaStreamSource(stream);
-        // Create the Recorder object and configure to record mono sound (1 channel) Recording 2 channels will double the file size
-        let rec = new Recorder(audioInput, {
-            numChannels: 1
-        })
-        // Start the recording process
-        rec.record()
-        recording = true
     }).catch((err) => {
-        console.error(`you got an error: ${err}`)
-        recording = false
+        console.error(`User refused microphone authorization: ${err}`)
+        return false
     });
+    return true
 }
 
 // ChatBox object
@@ -116,7 +83,7 @@ class ChatBox {
         textField.value = ''
         // Create an event listener on the button element:
         // Get the receiver endpoint from Python using fetch:
-        fetch("http://127.0.0.1:5000/chatbot-receiver",
+        fetch("http://localhost:5000/chatbot-receiver",
             {
                 method: 'POST',
                 // Stringify the payload into JSON:
@@ -144,41 +111,46 @@ class ChatBox {
     }
 
     // onSttButton
-    onSttButton() {
-        if (!recording) {
-            getLocalStream()
-        } else {
-            navigator.mediaDevices.getUserMedia({video: false, audio: false}).then()
-            // Stop the audio record
-            rec.stop();
-            recording = false
-            gumStream.getAudioTracks()[0].stop();
-            // Create the wav blob and pass it on to queryWhisperStt
-            rec.exportWAV((blob) => {
-                // Call whisper STT api
-                queryWhisperStt(blob).then((response) => {
-                    console.log(JSON.stringify(response));
+    onSttButton(chatbox) {
+        if (getLocalStream()) {
+            recognition.start();
+            console.log('Ready to receive an instruction');
+            recognition.onresult = (event) => {
+                const instruction = event.results[0][0].transcript;
+                // Publish on the channel of the user
+                let msg1 = {name: "User", message: instruction}
+                this.messages.push(msg1);
+                this.updateChatText(chatbox)
+                // Create an event listener on the button element:
+                // Get the receiver endpoint from Python using fetch:
+                fetch("http://localhost:5000/chatbot-receiver",
+                    {
+                        method: 'POST',
+                        // Stringify the payload into JSON:
+                        body: JSON.stringify({message: instruction}),
+                        mode: 'cors',
+                        headers: {
+                            'Content-type': 'application/json',
+                            'Accept': 'application/json'
+                        }
+                    }).then(r => r.json())
+                    .then(jsonResponse => {
+                        console.log(jsonResponse)
+                        let resp = jsonResponse["response"].replace("\n", "<br>");
+                        let msg2 = {name: "Nambot", message: resp};
+                        this.messages.push(msg2);
+                        this.updateChatText(chatbox)
+                        if (resp !== "") {
+                            vocal.text = resp.replace("<br>", " ");
+                            console.log(vocal.text)
+                            window.speechSynthesis.speak(vocal);
+                        }
+                    }).catch((error) => {
+                    console.error('Error:', error)
                 });
-            });
+            }
         }
     }
-
-    /* if (getLocalStream()) {
-        console.log("clicked microphone");
-        const SpeechRecognition = window.webkitSpeechRecognition;
-        const recognition = new SpeechRecognition();
-        recognition.onstart = () => {
-            console.log("starting listening, speak in microphone");
-        }
-        recognition.onspeechend = () => {
-            console.log("stopped listening");
-            recognition.stop();
-        }
-        recognition.onresult = (result) => {
-            console.log(result.results[0][0].transcript);
-        }
-        recognition.start();
-    } */
 
     // Is updating the ChatBot discussion fields
     updateChatText(chatbot) {
