3583c44383ffcf57b7e30b48c4be5fc48d7921bd
Max Lesage <max.lesage-ext@ima.eu>
2022-12-12 22:37:58 +0100

    updates

--- static/js/chess.js
+++ static/js/chess.js
@@ -1,22 +1,41 @@
 // Chess functions:
+
+let flag = false;
+let movesArray = [];
+let oldBg = "";
+
 function checkPossibleMoveForThisPiece(piece, game_id) {
-    const chess_case = piece.parentNode.id;
-    console.log(chess_case)
-    // Do a request to check possible move for this case.
-    fetch(
-        `http://localhost:5000/chess/${game_id}`,
-        {
-            method: 'POST',
-            body: JSON.stringify({chess_case: chess_case}),
-            mode: 'cors',
-            headers: {
-                'Content-type': 'application/json',
-                'Accept': 'application/json'
-            }
-        }).then(r => r.json())
-        .then(jsonResponse => {
-            console.log(jsonResponse)
-        }).catch((error) => {
-        console.error('Error:', error)
-    });
+    flag = !flag;
+    if (flag) {
+        const chessCase = piece.parentNode.id;
+        // Do a request to check possible move for this case:
+        fetch(
+            `http://localhost:5000/chess/${game_id}`,
+            {
+                method: 'POST',
+                body: JSON.stringify({chess_case: chessCase}),
+                mode: 'cors',
+                headers: {
+                    'Content-type': 'application/json',
+                    'Accept': 'application/json'
+                }
+            }).then(r => r.json())
+            .then(jsonResponse => {
+                // Put some light on the chess cases available for the piece selected:
+                movesArray = jsonResponse["possible_moves"];
+                for (const possibility of movesArray) {
+                    const elem = document.getElementById(possibility.slice(2, 4));
+                    elem.setAttribute("class", "col d-flex justify-content-center");
+                    elem.setAttribute("style", "background-color: #f8f9fa");
+                }
+            }).catch((error) => {
+            console.error('Error:', error)
+        });
+    } else {
+        for (const possibility of movesArray) {
+            const elem = document.getElementById(possibility.slice(2, 4));
+            elem.setAttribute("class", "col bg-secondary d-flex justify-content-center");
+        }
+        movesArray = [];
+    }
 }
