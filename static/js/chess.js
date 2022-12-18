e0e8ad9ec11348ef8300981f41a01f62ba6e1341
Max Lesage <max.lesage-ext@ima.eu>
2022-12-18 21:59:20 +0100

    Mouvements fonctionnels, les pièces peuvent se manger, manque le roc et un bug côté droit

--- static/js/chess.js
+++ static/js/chess.js
@@ -9,7 +9,7 @@ const primaryBgCases = [
 ];
 
 // When we want to set a case with an empty img after the move
-// const emptyImg =
+const emptyImg = document.querySelector('img[alt="empty"]');
 
 // Overridable var contributing to script logic
 let flag = false;
@@ -90,11 +90,10 @@ function removeLightOnCase(gameId) {
  * @param move
  */
 function forwardPiece(move) {
-    console.log(move);
     const fromChessCase = document.getElementById(move.slice(0, 2));
     const toChessCase = document.getElementById(move.slice(2, 4));
     toChessCase.removeChild(toChessCase.firstElementChild);
-    fromChessCase.appendChild(document.querySelector('img[alt="empty"]').cloneNode());
+    fromChessCase.appendChild(emptyImg.cloneNode());
     toChessCase.appendChild(fromChessCase.firstElementChild);
 }
 
